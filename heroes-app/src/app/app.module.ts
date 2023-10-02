910091af7aba6d6c0bae31cada965daa4ef3e7a4
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2023-10-02 17:57:13 -0500

    fix-db

--- heroes-app/src/app/app.module.ts
+++ heroes-app/src/app/app.module.ts
@@ -1,22 +1,21 @@
 //Components
 import { AppComponent } from './app.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListComponent } from './heroes/list/list.component';
 
 //Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    DbzModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
